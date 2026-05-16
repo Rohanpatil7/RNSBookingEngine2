da0949203024fe1d2612b38167d5270d69269a2f
Rohan Patil <107711621+Rohanpatil7@users.noreply.github.com>
2026-05-16 15:55:52 +0530

    Add new API proxy configurations in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,20 @@ export default ({ mode }) => {
       secure: false,
     },
 
+    
+    "/booking/api_bck": {
+      target: env.VITE_API_URL || "https://membership.xpresshotelpos.com/",
+      changeOrigin: true,
+      secure: true,
+      rewrite: (path) => path.replace(/^\/booking\/api_bck/, "/booking/api_bck"),
+    },
 
+    "/pg_demo": {
+      target: "https://membership.xpresshotelpos.com/",
+      changeOrigin: true,
+      secure: true,
+      rewrite: (path) => path.replace(/^\/pg_demo/, "/pg_demo"),
+    },
     
   };
 
